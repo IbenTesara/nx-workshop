0d971de7463080eef26a9d10816222a468e7a0d9
Iben Van de Veire <iben.vandeveire@studiohyperdrive.be>
2023-03-23 14:38:35 +0100

    add affected test

--- apps/api/src/app/app.service.spec.ts
+++ apps/api/src/app/app.service.spec.ts
@@ -15,7 +15,7 @@ describe('AppService', () => {
 
   describe('getData', () => {
     it('should return "Welcome to api!"', () => {
-      expect(service.getData()).toEqual({ message: 'Welcome to api!' });
+      expect(service.getGame('settlers-in-the-can').name).toEqual('Settlers in the Can');
     });
   });
 });
